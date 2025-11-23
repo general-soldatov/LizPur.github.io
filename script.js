c4bf7bf30db8cfb68dc5e2a02424fe06f945713e
general-soldatov <general-soldatov@mail.ru>
2025-11-23 18:44:37 +0300

    Sample update

--- script.js
+++ script.js
@@ -15,18 +15,18 @@ function phusic(){
    let mater = select.value;
    if (mater === 'copper') {
         gravity = (size * 8.92 * 10)/1000;
-        alert(gravity + ' H сила тяжести маятника');
-}  else if (mater === 'silver') {
+        // alert(gravity + ' H сила тяжести маятника');
+    }  else if (mater === 'silver') {
         gravity = (size * 10.49 * 10)/1000;
-        alert(gravity + ' H сила тяжести маятника');
-}  else if (mater === 'iron') {
+        // alert(gravity + ' H сила тяжести маятника');
+    }  else if (mater === 'iron') {
         gravity = (size * 7.87 * 10)/1000;
-        alert(gravity + ' H сила тяжести маятника');
     }
+    alert(gravity + ' H сила тяжести маятника');
 }
 
 window.onload = () => {
   const button = document.querySelector('#phusicbutton');
-  button.addEventListener('click', calculate);
+//   button.addEventListener('click', calculate);
   button.addEventListener('click', phusic);
 };
