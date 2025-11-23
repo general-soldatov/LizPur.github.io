958e77fffd6cc6cdf4270edad05fde19f2a4404b
LizPur <shmatov.vladimir07@mail.ru>
2025-11-23 17:04:09 +0300

    'creat'

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+let angleradians;
 function calculate() {
     let anglenew = document.querySelector('#angle').value;
     if (anglenew) {
-        let angleradians = anglenew * Math.PI / 180;
+        angleradians = anglenew * Math.PI / 180;
         alert(`Результат: ${angleradians.toFixed(2)}`);
     }
 }
-
+function material_m(){
+   let select = document.querySelector('select');
+   let mater = select.value;
+   if (mater === 'copper'){
+        alert(angleradians * 1000)
+   }
+   if (mater === 'silver'){
+        alert(angleradians * 0)
+   }
+   if (mater === 'iron'){
+        alert(angleradians * 1)
+   }
+}
 window.onload = () => {
-  document.querySelector('#phusicbutton').addEventListener('click', calculate);
+  const button = document.querySelector('#phusicbutton');
+  button.addEventListener('click', calculate);
+  button.addEventListener('click', material_m);
 };
