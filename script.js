3724463689c8841b447399f7fe825c35a9579301
LizPur <shmatov.vladimir07@mail.ru>
2025-11-24 17:49:38 +0300

    'creat'

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let angleradians;
 
 function calculate() {
     let anglenew = document.querySelector('#angle').value;
+    let label_angle = document.querySelector('#print_angle')
     if (anglenew) {
-        angleradians = anglenew * Math.PI / 180;
-        alert(`Результат: ${angleradians.toFixed(2)}`);
+        let angleradians = anglenew * Math.PI / 180;
+        label_angle.textContent = 'Угол в градусах - '+ angleradians;
     }
 }
 
@@ -13,15 +13,18 @@ function phusic(){
    let gravity;
    let select = document.querySelector('select');
    let mater = select.value;
+   let label_gravity = document.querySelector('#print_gravity');
    if (mater === 'copper') {
         gravity = (size * 8.92 * 10)/1000;
+        label_gravity.textContent = 'Сила тяжести '+ gravity + ' Ньютонов';
 }  else if (mater === 'silver') {
         gravity = (size * 10.49 * 10)/1000;
+        label_gravity.textContent = 'Сила тяжести '+ gravity + ' Ньютонов';
 }  else if (mater === 'iron') {
         gravity = (size * 7.87 * 10)/1000;
+        label_gravity.textContent = 'Сила тяжести '+ gravity + ' Ньютонов';
     }
-    alert(gravity + ' H сила тяжести маятника');
-}
+ }
 
 window.onload = () => {
   const button = document.querySelector('#phusicbutton');
